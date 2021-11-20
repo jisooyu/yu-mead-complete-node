24f7a97bde612f0ebde01c6c7d1f071528788f6c
jisooyu <jisooyu@hotmail.com>
2021-11-20 19:50:23 +0900

    first lecture

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique:true,
+        required: true,
+        trim:true ,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid")
+            }
+        }
+    },
+    password:{
+        type:String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error ('Password can not contain "password" ')
+            }
+        }
+    }
+})
+
+userSchema.pre('save', async function (next){
+    const user = this
+
+    if(user.isModified ('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
